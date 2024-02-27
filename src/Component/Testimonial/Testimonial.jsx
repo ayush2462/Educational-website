60e6b9cbd57b19387a2b3bdf9ef7cf75c8fcb4d0
ayush2462 <128534675+ayush2462@users.noreply.github.com>
2024-02-27 15:45:39 +0530

    adding slider

--- src/Component/Testimonial/Testimonial.jsx
+++ src/Component/Testimonial/Testimonial.jsx
@@ -5,13 +5,34 @@ import user1 from "../../assets/user-1.png";
 import user2 from "../../assets/user-2.png";
 import user3 from "../../assets/user-3.png";
 import user4 from "../../assets/user-4.png";
+import { useRef } from "react";
 const Testimonial = () => {
+  const slider = useRef();
+  let tx = 0;
+
+  const slideForward = () => {
+    if (tx > -50) {
+      tx -= 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  };
+  const slideBackward = () => {
+    if (tx < 0) {
+      tx += 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`;
+  };
   return (
     <div className="testimonial">
-      <img src={next_icon} alt="" className="next-btn" />
-      <img src={back_icon} alt="" className="back-btn" />
+      <img src={next_icon} alt="" className="next-btn" onClick={slideForward} />
+      <img
+        src={back_icon}
+        alt=""
+        className="back-btn"
+        onClick={slideBackward}
+      />
       <div className="slider">
-        <ul>
+        <ul ref={slider}>
           <li>
             <div className="slide">
               <div className="user-info">
@@ -21,7 +42,11 @@ const Testimonial = () => {
                   <span>Edusity,USA</span>
                 </div>
               </div>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus veniam alias tempora? Nobis soluta sit perspiciatis ea aliquam aspernatur. Enim.</p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus veniam alias tempora? Nobis soluta sit perspiciatis ea
+                aliquam aspernatur. Enim.
+              </p>
             </div>
           </li>
           <li>
@@ -33,7 +58,11 @@ const Testimonial = () => {
                   <span>Edusity,USA</span>
                 </div>
               </div>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus veniam alias tempora? Nobis soluta sit perspiciatis ea aliquam aspernatur. Enim.</p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus veniam alias tempora? Nobis soluta sit perspiciatis ea
+                aliquam aspernatur. Enim.
+              </p>
             </div>
           </li>
           <li>
@@ -45,7 +74,11 @@ const Testimonial = () => {
                   <span>Edusity,USA</span>
                 </div>
               </div>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus veniam alias tempora? Nobis soluta sit perspiciatis ea aliquam aspernatur. Enim.</p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus veniam alias tempora? Nobis soluta sit perspiciatis ea
+                aliquam aspernatur. Enim.
+              </p>
             </div>
           </li>
           <li>
@@ -57,7 +90,11 @@ const Testimonial = () => {
                   <span>Edusity,USA</span>
                 </div>
               </div>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus veniam alias tempora? Nobis soluta sit perspiciatis ea aliquam aspernatur. Enim.</p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus veniam alias tempora? Nobis soluta sit perspiciatis ea
+                aliquam aspernatur. Enim.
+              </p>
             </div>
           </li>
         </ul>
